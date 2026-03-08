a2ae79cae7d6bd466419f9fb64ea44860c88b5c2
motebaya <davinsexo722@gmail.com>
2026-03-08 09:21:15 +0700

    chore: bump version to 2.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.motebaya.vaulten"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "2.2.0"
+        versionCode = 9
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
